384e03cde8b4f3e554f6b692bdcc53ab03f6a9b1
extobias <extobias@gmail.com>
2013-12-13 17:07:04 -0300

    test 02

---
+++ src/test001/Calculadora.java
@@ -0,0 +1,15 @@
+package test001;
+
+public class Calculadora {
+
+	public Calculadora() {
+
+		System.out.println("Calculadora inicializada por Spring !!");
+	}
+
+public int sumar (int numero1, int numero2){
+
+		return numero1 + numero2;
+	}
+		 
+}
